b583d4d86ee526baaf37a0ea86e5ef89285b5e89
Lachlan <lachlan.hughes@my.jcu.edu.au>
2017-03-24 09:49:53 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,3 +1,16 @@
 """
 example docstring
 """
+
+
+name = input("Enter your name: ")
+
+while not name:
+    print("Name cannot be blank")
+    name = input("Enter your name: ")
+
+print(name)
+
+for i in range(len(name)):
+    if i%2 == 1:
+        print(name[i])
